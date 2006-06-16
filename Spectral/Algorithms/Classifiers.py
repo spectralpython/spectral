717992a824c7b9877faa6f6673ff3ca87808f54b
Thomas Boggs <tboggs@users.sourceforge.net>
2006-06-16 17:47:15 +0000

    Changed typecode of classification map in Classifier.classifiyImage. Added copyright header. Added MahalanobisDistanceClassifier.

--- Spectral/Algorithms/Classifiers.py
+++ Spectral/Algorithms/Classifiers.py
@@ -1,3 +1,33 @@
+#########################################################################
+#
+#   Classifiers.py - This file is part of the Spectral Python (SPy)
+#   package.
+#
+#   Copyright (C) 2001-2006 Thomas Boggs
+#
+#   Spectral Python is free software; you can redistribute it and/
+#   or modify it under the terms of the GNU General Public License
+#   as published by the Free Software Foundation; either version 2
+#   of the License, or (at your option) any later version.
+#
+#   Spectral Python is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#     
+#   You should have received a copy of the GNU General Public License
+#   along with this software; if not, write to
+#
+#               Free Software Foundation, Inc.
+#               59 Temple Place, Suite 330
+#               Boston, MA 02111-1307
+#               USA
+#
+#########################################################################
+#
+# Send comments to:
+# Thomas Boggs, tboggs@users.sourceforge.net
+#
 
 class Classifier:
     '''
@@ -14,7 +44,7 @@ class Classifier:
         from Numeric import zeros
         status.displayPercentage('Classifying image...')
         it = ImageIterator(image)
-        classMap = zeros(image.shape[:2], Int0)
+        classMap = zeros(image.shape[:2])
         N = it.getNumElements()
         i, inc = (0, N / 100)
         for spectrum in it:
@@ -67,7 +97,6 @@ class GaussianClassifier(SupervisedClassifier):
 
         ARGUMENTS:
             x           The spectrum to classify
-            classes     A list of TrainingSet objects with stats
         RETURN VALUE
             classIndex  The 'index' property of the most likely class
                         in classes.
@@ -91,3 +120,51 @@ class GaussianClassifier(SupervisedClassifier):
                 maxProb = prob[0,0]
                 maxClass = self.classes[i].index
         return maxClass
+
+class MahalanobisDistanceClassifier(GaussianClassifier):
+    '''A Classifier using Mahalanobis distance for class discrimination'''
+    def train(self, trainingData):
+        '''
+        Calculate a single coveriance as a weighted average of the
+        individual training class covariances.
+        '''
+        import Numeric
+        from LinearAlgebra import inverse
+        GaussianClassifier.train(self, trainingData)
+
+        covariance = Numeric.zeros(self.classes[0].stats.cov.shape, Numeric.Float)
+        numSamples = 0
+        for cl in self.classes:
+            covariance += cl.stats.numSamples * cl.stats.cov
+            numSamples += cl.stats.numSamples
+        self.invCovariance = inverse(covariance / numSamples)
+
+    def classifySpectrum(self, x):
+        '''
+        Classify pixel based on minimum Mahalanobis distance.
+
+        USAGE: classIndex = classifier.classifySpectrum(x)
+
+        ARGUMENTS:
+            x           The spectrum to classify
+        RETURN VALUE
+            classIndex  The 'index' property of the most likely class
+                        in classes.
+        '''
+        from Numeric import NewAxis, matrixmultiply, transpose
+ 
+        maxClass = -1
+        d2_min = -1
+
+        for i in range(len(self.classes)):
+            cl = self.classes[i]
+            delta = (x - cl.stats.mean)[:, NewAxis]
+            d2 = matrixmultiply(transpose(delta), matrixmultiply(self.invCovariance, delta))
+            if i == 0:
+                d2_min = d2
+                maxClass = self.classes[0].index
+            elif (d2 < d2_min):
+                d2_min = d2
+                maxClass = self.classes[i].index
+        return maxClass
+
