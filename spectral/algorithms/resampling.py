0a8cc548b0a092450a97aeeeaa2fdeabd41f17ba
Thomas Boggs <thomas.boggs@gmail.com>
2017-07-02 15:36:48 -0400

    Fall back to local erf if no other is available.

--- spectral/algorithms/resampling.py
+++ spectral/algorithms/resampling.py
@@ -34,10 +34,32 @@ Functions for resampling a spectrum from one band discretization to another.
 
 from __future__ import division, print_function, unicode_literals
 
+def erf_local(x):
+    import math
+    # save the sign of x
+    sign = 1 if x >= 0 else -1
+    x = abs(x)
+
+    # constants
+    a1 =  0.254829592
+    a2 = -0.284496736
+    a3 =  1.421413741
+    a4 = -1.453152027
+    a5 =  1.061405429
+    p  =  0.3275911
+
+    # A&S formula 7.1.26
+    t = 1.0/(1.0 + p*x)
+    y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*math.exp(-x*x)
+    return sign*y # erf(-x) = -erf(x)
+
 try:
     from math import erf
 except:
-    from scipy.special import erf
+    try:
+        from scipy.special import erf
+    except:
+        erf = erf_local
 
 def erfc(z):
     '''Complement of the error function.'''
