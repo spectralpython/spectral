cec7eb94c697f70509093c30a456a412cfda0583
Marko Ivanovic <ivanovic.marko@yandex.com>
2020-06-05 17:08:06 +0000

    Implement segmented upper hull

--- spectral/algorithms/continuum.py
+++ spectral/algorithms/continuum.py
@@ -16,6 +16,10 @@ References:
     on the continuum-removed," Proc. SPIE 4897, Multispectral and Hyperspectral
     Remote Sensing Instruments and Applications, (16 June 2003);
     doi: 10.1117/12.466729
+
+    Lehnert, Lukas & Meyer, Hanna & Obermeier, Wolfgang & Silva, Brenner & Regeling,
+    Bianca & Thies, Boris & Bendix, Jorg. (2019). Hyperspectral Data Analysis in R:
+    The hsdar Package. Journal of statistical software. 89. 1-23. 10.18637/jss.v089.i12.
 '''
 
 from __future__ import absolute_import, division, print_function, unicode_literals
@@ -26,7 +30,86 @@ import numpy as np
 import spectral as spy
 from ..utilities.errors import has_nan, NaNValueError
 
-def _find_indices_in_range(spectrum, bands, indices, ind_fill, ibegin, iend):
+
+def _segment_concave_region(spectrum, bands, indices, ind_fill, ibegin, iend):
+    # Here we don't search for local maxima w.r.t. line that connects ends of this region.
+    # That is behavior of the hsdar. It also makes more sense in the context of
+    # hyperspectral image analysis. We are already not convex, and we can't
+    # include all points that make result quasi-convex, since there will be too
+    # many of them, often right one after another. However, filtering local maxima,
+    # below, will make result quasi-convex.
+    # Notice that we are using >=, not strict >. That will include maxima that
+    # are flat, that stretch few points. It will also include local minima,
+    # but just as with local maxima that are too low below slope line, these
+    # will be filtered out.
+    is_maximum = np.logical_and(spectrum[ibegin+1:iend-1] >= spectrum[ibegin:iend-2],
+                                spectrum[ibegin+1:iend-1] >= spectrum[ibegin+2:iend])
+
+    # Get local maxima indices. (Note that where return tuple for each dimension).
+    lmi = np.where(is_maximum)[0]
+
+    # No local maxima, return.
+    if len(lmi) == 0:
+        return ind_fill
+
+    # Make it relative to input array - spectrum.
+    lmi += ibegin + 1
+
+    # Get local maxima.
+    local_maxima = spectrum[lmi]
+
+    # Filter those maxima that cause slope between them to change direction.
+    # This makes remaining maxima, satisfy quasy-convexity condition.
+    slope_dir = spectrum[iend-1] - spectrum[ibegin]
+    filtered_indices = []
+    if slope_dir >= 0.0:
+        last_included_value = spectrum[ibegin]
+        for i in range(len(local_maxima)):
+            lm = local_maxima[i]
+            if lm > last_included_value:
+                filtered_indices.append(lmi[i])
+                last_included_value = lm
+    else:
+        # Slope is negative. Start from back.
+        last_included_value = spectrum[iend-1]
+        for i in range(len(local_maxima) - 1, -1, -1):
+            lm = local_maxima[i]
+            if lm > last_included_value:
+                filtered_indices.append(lmi[i])
+                last_included_value = lm
+        filtered_indices.reverse()
+
+    # Take only valid local maxima indices.
+    lmi = filtered_indices
+
+    # If there is no valid local maxima indices, return.
+    if len(lmi) == 0:
+        return ind_fill
+
+    # Add indices to result, and process subregions between them with convex hull
+    # algorithm, to make sure all input points and below resulting hull.
+    next_ibegin = ibegin
+    for i in lmi:
+        # There should be at least 1 point between edges, to call _find_indices_in_range.
+        # However, these are to local maxima, and if there is one point between them,
+        # it must be below both. So only for two points inside region borders
+        # call _find_indices_in_range.
+        if i > next_ibegin + 2:
+            # Put hull around points in subregion.
+            ind_fill = _find_indices_in_range(
+                spectrum, bands, False, indices, ind_fill, next_ibegin, i + 1)
+        indices[ind_fill] = i
+        ind_fill += 1
+        next_ibegin = i
+
+    # Don't miss the last range.
+    ind_fill = _find_indices_in_range(
+        spectrum, bands, False, indices, ind_fill, lmi[-1], iend)
+
+    return ind_fill
+
+
+def _find_indices_in_range(spectrum, bands, segmented, indices, ind_fill, ibegin, iend):
     iendi = iend - 1
 
     # We search for maximum, but not from the x axis.
@@ -44,17 +127,24 @@ def _find_indices_in_range(spectrum, bands, indices, ind_fill, ibegin, iend):
     # we could divide each dot product by norm/length of the normal. But that is constant,
     # and does not effect which one is maximum.
     # Note that here we include first point of the range, but not last.
-    imax = np.argmax(bands[ibegin:iendi] * naxis_x + spectrum[ibegin:iendi] * naxis_y) + ibegin
+    imax = np.argmax(bands[ibegin:iendi] * naxis_x +
+                     spectrum[ibegin:iendi] * naxis_y) + ibegin
 
     # If first point is maximum, then all others are "below" the axis,
     # which means this is concave region.
     if imax == ibegin:
+        # If we are in segmented upper hull mode, then segment concave region.
+        # For that to make sense, we need at least 3 elements between edges.
+        if segmented and iend - ibegin > 5:
+            ind_fill = _segment_concave_region(
+                spectrum, bands, indices, ind_fill, ibegin, iend)
         return ind_fill
 
     # Repeat same procedure on the left side, if there are enough points left.
     # At least 1 is required between first and last point in range.
     if imax > ibegin + 1:
-        ind_fill = _find_indices_in_range(spectrum, bands, indices, ind_fill, ibegin, imax + 1)
+        ind_fill = _find_indices_in_range(
+            spectrum, bands, segmented, indices, ind_fill, ibegin, imax + 1)
 
     # Push middle index.
     indices[ind_fill] = imax
@@ -62,22 +152,26 @@ def _find_indices_in_range(spectrum, bands, indices, ind_fill, ibegin, iend):
 
     # Repeat for the right side.
     if imax < iend - 2:
-        ind_fill =_find_indices_in_range(spectrum, bands, indices, ind_fill, imax, iend)
+        ind_fill = _find_indices_in_range(
+            spectrum, bands, segmented, indices, ind_fill, imax, iend)
 
     return ind_fill
 
-def _find_continuum_points_recursive(spectrum, bands, indices):
+
+def _find_continuum_points_recursive(spectrum, bands, segmented, indices):
     n = len(spectrum)
     indices[0] = 0
     ind_fill = 1
 
-    ind_fill = _find_indices_in_range(spectrum, bands, indices, ind_fill, 0, n)
+    ind_fill = _find_indices_in_range(
+        spectrum, bands, segmented, indices, ind_fill, 0, n)
     indices[ind_fill] = n - 1
     indices = indices[:ind_fill + 1]
 
     return (bands[indices], spectrum[indices])
 
-def _process_continuum(spectra, bands, remove_continuum, out):
+
+def _process_continuum(spectra, bands, remove_continuum, segmented, out):
     if not isinstance(spectra, np.ndarray):
         raise TypeError('Expected spectra to be a numpy.ndarray.')
     if not isinstance(bands, np.ndarray):
@@ -93,7 +187,7 @@ def _process_continuum(spectra, bands, remove_continuum, out):
 
     out = np.empty_like(spectra) if out is None else out
 
-    # In case we remove continuum, always devide out by continuum,
+    # In case we remove continuum, always divide out by continuum,
     # to avoid creating additional temporary array.
     if spectra is not out and remove_continuum:
         out[:] = spectra[:]
@@ -105,7 +199,8 @@ def _process_continuum(spectra, bands, remove_continuum, out):
     indices = np.empty(nbands, np.int64)
 
     if len(spectra.shape) == 1:
-        points = _find_continuum_points_recursive(spectra, bands, indices)
+        points = _find_continuum_points_recursive(
+            spectra, bands, segmented, indices)
         continuum = interp(bands, points[0], points[1])
         if remove_continuum:
             out /= continuum
@@ -113,7 +208,8 @@ def _process_continuum(spectra, bands, remove_continuum, out):
             out[:] = continuum
     elif len(spectra.shape) == 2:
         for i in range(spectra.shape[0]):
-            points = _find_continuum_points_recursive(spectra[i], bands, indices)
+            points = _find_continuum_points_recursive(
+                spectra[i], bands, segmented, indices)
             continuum = interp(bands, points[0], points[1])
             if remove_continuum:
                 out[i, :] /= continuum
@@ -122,7 +218,8 @@ def _process_continuum(spectra, bands, remove_continuum, out):
     else:
         for i in range(spectra.shape[0]):
             for j in range(spectra.shape[1]):
-                points = _find_continuum_points_recursive(spectra[i, j], bands, indices)
+                points = _find_continuum_points_recursive(
+                    spectra[i, j], bands, segmented, indices)
                 continuum = interp(bands, points[0], points[1])
                 if remove_continuum:
                     out[i, j, :] /= continuum
@@ -131,13 +228,14 @@ def _process_continuum(spectra, bands, remove_continuum, out):
 
     return out
 
-def continuum_points(spectrum, bands):
+
+def continuum_points(spectrum, bands, mode='convex'):
     '''Returns points of spectra that belong to it's continuum.
 
     Arguments:
 
         `spectrum` (:class:`numpy.ndarray`)
-        
+
             1d :class:`numpy.ndarray` holding spectral signature.
 
         `bands` (:class:`numpy.ndarray`):
@@ -145,8 +243,15 @@ def continuum_points(spectrum, bands):
             1d :class:`numpy.ndarray`, holding band values of spectra.
             Length of `bands` should be the same as `spectrum`.
 
+        `mode` (string, default 'convex'):
+
+            Default mode is 'convex' which returns convex upper hull of the
+            spectrum. Another supported mode is 'segmented' which builds
+            segmented upper hull. This is usefull to identify more detailed
+            contour of the spectrum, but without strong absorption bands.
+
     Returns:
-    
+
         2-tuple, with each element being :class:`numpy.ndarray`.
         First element contains reflectance values of points that belong to
         continuum. Second element contains corresponding bands.
@@ -154,26 +259,27 @@ def continuum_points(spectrum, bands):
         continuum of spectrum. However this function is particularly useful to
         applying other interpolations or any other processing on these points.
     '''
-    if not isinstance(spectra, np.ndarray):
+    if not isinstance(spectrum, np.ndarray):
         raise TypeError('Expected spectra to be a numpy.ndarray.')
     if not isinstance(bands, np.ndarray):
         raise TypeError('Expected bands to be a numpy.ndarray.')
-    if len(spectra.shape) != 1:
+    if len(spectrum.shape) != 1:
         raise ValueError('Expected spectra to be 1d array.')
     if len(bands.shape) != 1:
         raise ValueError('Expected bands to be 1d array.')
 
     indices = np.empty_like(spectrum, dtype='int64')
-    return _find_continuum_points_recursive(spectrum, bands, indices)
+    return _find_continuum_points_recursive(spectrum, bands, mode == 'segmented', indices)
 
-def spectral_continuum(spectra, bands, out = None):
+
+def spectral_continuum(spectra, bands, mode='convex', out=None):
     '''Returns continua of spectra.
     Continuum is defined as convex hull of spectra.
 
     Arguments:
 
         `spectra` (:class:`numpy.ndarray`)
-        
+
             Can be 1d, 2d or 3d :class:`numpy.ndarray`, where last dimension
             holds individual spectra.
 
@@ -181,6 +287,13 @@ def spectral_continuum(spectra, bands, out = None):
 
             1d :class:`numpy.ndarray`, holding band values of spectra.
             Length of `bands` should be the same as last dimension of `spectra`.
+
+        `mode` (string, default 'convex'):
+
+            Default mode is 'convex' which returns convex upper hull of the
+            spectrum. Another supported mode is 'segmented' which builds
+            segmented upper hull. This is usefull to identify more detailed
+            contour of the spectrum, but without strong absorption bands.
 
         `out` (:class:`numpy.ndarray`, default None):
 
@@ -189,14 +302,15 @@ def spectral_continuum(spectra, bands, out = None):
             function.
 
     Returns:
-    
+
         A :class:`numpy.ndarray` of continua for each spectrum in spectra.
         It same type and shape as spectra. If `out` is provided, `out` will be
         returned.
     '''
-    return _process_continuum(spectra, bands, False, out)
+    return _process_continuum(spectra, bands, False, mode == 'segmented', out)
 
-def remove_continuum(spectra, bands, out = None):
+
+def remove_continuum(spectra, bands, mode='convex', out=None):
     '''Returns spectra with continuum removed.
     Continuum is defined as convex hull of spectra. Continuum is removed from
     spectra by deviding spectra by its continuum.
@@ -204,7 +318,7 @@ def remove_continuum(spectra, bands, out = None):
     Arguments:
 
         `spectra` (:class:`numpy.ndarray`)
-        
+
             Can be 1d, 2d or 3d :class:`numpy.ndarray`, where last dimension
             holds individual spectra.
 
@@ -213,6 +327,13 @@ def remove_continuum(spectra, bands, out = None):
             1d :class:`numpy.ndarray`, holding band values of spectra.
             Length of `bands` should be the same as last dimension of `spectra`.
 
+        `mode` (string, default 'convex'):
+
+            Default mode is 'convex' which removes convex upper hull of the
+            spectrum. Another supported mode is 'segmented' which removes
+            segmented upper hull. This is usefull to identify two or more small
+            features instead of one large feature.
+
         `out` (:class:`numpy.ndarray`, default None):
 
             If provided, it must have type `np.float64` and same shape as
@@ -220,12 +341,12 @@ def remove_continuum(spectra, bands, out = None):
             result of this function.
 
     Returns:
-    
+
         A :class:`numpy.ndarray` of continua for in spectrum in spectra.
         It type `np.float64` and same shape as spectra. If `out` is provided,
         `out` will be returned.
     '''
     if out is not None and out.dtype != np.float64:
         raise ValueError('Expected out to have dtype float64. '
-                        'Results of continuum removal are floating point numbers.')
-    return _process_continuum(spectra, bands, True, out)
+                         'Results of continuum removal are floating point numbers.')
+    return _process_continuum(spectra, bands, True, mode == 'segmented', out)
--- spectral/tests/continuum.py
+++ spectral/tests/continuum.py
@@ -51,6 +51,102 @@ class FindContinuumTest(ContinuumTest):
         expected = np.array([0.5, 1.1, 1.8, 2., 1.8625, 0.9, 0.4])
         assert_allclose(expected, spectral_continuum(spectrum, bands))
 
+    def test_simple_segmented(self):
+        # A case without local maximum inside concave regions.
+        # Same as convex hull.
+        spectrum = np.array([0.6, 1., 2.45, 3.1, 3.25, 4.15,
+                             4.35, 4.1, 3.1, 2.7, 2., 2.85, 3.75, 3., 2., 0.9])
+        bands = np.array([0.3, 1., 1.8, 3., 4.5, 5.2, 6.45,
+                          7., 7.1, 8., 8.1, 9., 9.3, 10.2, 10.5, 10.6])
+        expected = np.array([0.6, 1.46333333, 2.45, 3.1, 3.81590909, 4.15, 4.35,
+                             4.23421053, 4.21315789, 4.02368421, 4.00263158, 3.81315789, 3.75, 3.,
+                             2., 0.9, ])
+        assert_allclose(expected, spectral_continuum(
+            spectrum, bands, 'segmented'))
+
+        # A case with single local maxima that gets filtered out, because it
+        # does not satisfy quasi-convexity.
+        spectrum = np.array([0.6, 1., 2.45, 3.1, 3.25, 4.15,
+                             4.35, 4.1, 3.1, 3.7, 2., 2.85, 3.75, 3., 2., 0.9])
+        bands = np.array([0.3, 1., 1.8, 3., 4.5, 5.2, 6.45,
+                          7., 7.1, 8., 8.1, 9., 9.3, 10.2, 10.5, 10.6])
+        expected = np.array([0.6, 1.46333333, 2.45, 3.1, 3.81590909,
+                             4.15, 4.35, 4.23421053, 4.21315789, 4.02368421,
+                             4.00263158, 3.81315789, 3.75, 3., 2.,
+                             0.9])
+        assert_allclose(expected, spectral_continuum(
+            spectrum, bands, 'segmented'))
+
+        # Reversed case.
+        spectrum = spectrum[::-1]
+        bands = np.cumsum(np.concatenate(
+            (np.array([0.3]), (bands[1:] - bands[:-1])[::-1])))
+        expected = expected[::-1]
+        assert_allclose(expected, spectral_continuum(
+            spectrum, bands, 'segmented'))
+
+        # A case with single valid local maxima inside concave region,
+        # and one invalid.
+        spectrum = np.array([0.60, 1.00, 2.45, 3.10, 3.25, 4.15, 4.35, 4.10,
+                             3.10, 3.80, 3.50, 3.60, 2.00, 2.85, 3.75, 3.00, 2.00, 0.90])
+        bands = np.array([0.30, 1.00, 1.80, 3.00, 4.50, 5.20, 6.45, 7.00,
+                          7.10, 8.00, 8.03, 8.07, 8.10, 9.00, 9.30, 10.20, 10.50, 10.6])
+        expected = np.array([0.6, 1.46333333, 2.45, 3.1, 3.81590909,
+                             4.15, 4.35, 4.15483871, 4.11935484, 3.8,
+                             3.79884615, 3.79730769, 3.79615385, 3.76153846, 3.75,
+                             3., 2., 0.9])
+        assert_allclose(expected, spectral_continuum(
+            spectrum, bands, 'segmented'))
+
+        # Reversed case.
+        spectrum = spectrum[::-1]
+        bands = np.cumsum(np.concatenate(
+            (np.array([0.3]), (bands[1:] - bands[:-1])[::-1])))
+        expected = expected[::-1]
+        assert_allclose(expected, spectral_continuum(
+            spectrum, bands, 'segmented'))
+
+        # A case with two valid local maxima.
+        spectrum = np.array([0.60, 1.00, 2.45, 3.10, 3.25, 4.15, 4.35, 4.10,
+                             3.10, 3.80, 3.50, 3.75, 2.00, 2.85, 3.70, 3.00, 2.00, 0.90])
+        bands = np.array([0.30, 1.00, 1.80, 3.00, 4.50, 5.20, 6.45, 7.00,
+                          7.10, 8.00, 8.03, 8.07, 8.10, 9.00, 9.30, 10.20, 10.50, 10.6])
+        expected = np.array([0.6, 1.46333333, 2.45, 3.1, 3.81590909,
+                             4.15, 4.35, 4.15483871, 4.11935484, 3.8,
+                             3.77857143, 3.75, 3.74878049, 3.71219512, 3.7,
+                             3., 2., 0.9])
+        assert_allclose(expected, spectral_continuum(
+            spectrum, bands, 'segmented'))
+
+        # Reversed case.
+        spectrum = spectrum[::-1]
+        bands = np.cumsum(np.concatenate(
+            (np.array([0.3]), (bands[1:] - bands[:-1])[::-1])))
+        expected = expected[::-1]
+        assert_allclose(expected, spectral_continuum(
+            spectrum, bands, 'segmented'))
+
+        # A case with two valid local maxima, but one covering eliminating the
+        # other.
+        spectrum = np.array([0.60, 1.00, 2.45, 3.10, 3.25, 4.15, 4.35, 4.10,
+                             3.10, 3.80, 3.50, 3.85, 2.00, 2.85, 3.70, 3.00, 2.00, 0.90])
+        bands = np.array([0.30, 1.00, 1.80, 3.00, 4.50, 5.20, 6.45, 7.00,
+                          7.10, 8.00, 8.03, 8.07, 8.10, 9.00, 9.30, 10.20, 10.50, 10.6])
+        expected = np.array([0.6, 1.46333333, 2.45, 3.1, 3.81590909,
+                             4.15, 4.35, 4.18024691, 4.14938272, 3.87160494,
+                             3.86234568, 3.85, 3.84634146, 3.73658537, 3.7,
+                             3., 2., 0.9])
+        assert_allclose(expected, spectral_continuum(
+            spectrum, bands, 'segmented'))
+
+        # Reversed case.
+        spectrum = spectrum[::-1]
+        bands = np.cumsum(np.concatenate(
+            (np.array([0.3]), (bands[1:] - bands[:-1])[::-1])))
+        expected = expected[::-1]
+        assert_allclose(expected, spectral_continuum(
+            spectrum, bands, 'segmented'))
+
     def test_2d_array(self):
         part = self.image[20:22, 20:22].reshape(4, 220)
         cnt = spectral_continuum(part, self.bands)
@@ -87,6 +183,26 @@ class FindContinuumPointsTest(ContinuumTest):
         assert(np.array_equal(points[1], np.array(
             [3505, 4141, 4516, 4924, 5002, 4712, 1019], dtype=np.int16)))
 
+    def test_points_of_real_spectrum_segmented(self):
+        # This example includes flat local maxima, that span three or more points.
+        points = continuum_points(self.image[20, 20], self.bands, 'segmented')
+        expected_result = (
+            np.array([400.019989,  409.820007,  419.619995,  449.070007,  458.899994,
+                      783.27002,  802.530029,  841.039978,  860.280029,  879.530029,
+                      994.880005, 1014.090027, 1052.48999, 1244.26001, 1273.,
+                      1282.959961, 1541.589966, 1561.439941, 1620.97998, 1630.900024,
+                      2122.780029, 2132.72998, 2172.5, 2212.219971, 2222.149902,
+                      2311.350098, 2331.139893, 2360.810059, 2390.459961, 2400.330078,
+                      2429.949951, 2459.540039, 2498.959961]),
+            np.array([3505, 4141, 4516, 4924, 5002, 4712, 4578, 4496, 4424, 4423, 3979,
+                      3925, 3801, 3026, 2852, 2817, 2206, 2204, 2136, 2124, 1377, 1376,
+                      1333, 1317, 1312, 1219, 1202, 1162, 1126, 1122, 1100, 1068, 1019],
+                     dtype=np.int16)
+        )
+        assert(np.array_equal(points[0], expected_result[0]))
+        assert(np.array_equal(points[1], expected_result[1]))
+
+
 class RemoveContinuumTest(ContinuumTest):
     '''Tests remove_continuum.'''
 
@@ -99,6 +215,18 @@ class RemoveContinuumTest(ContinuumTest):
         spectrum = np.array([0.60, 1.00, 2.45, 3.10, 3.25, 4.15, 4.35, 4.10, 3.10,
                              2.70, 2.00, 2.85, 3.75, 3.00, 2.00, 0.90])
         assert_allclose(continuum_removed, remove_continuum(spectrum, bands))
+
+    def test_simple_case_segmented(self):
+        continuum_removed = np.array([1., 0.6833713, 1., 1., 0.85169744,
+                                      1., 1., 0.98680124, 0.75254503, 1.,
+                                      0.52684904, 0.75766871, 1., 1., 1.,
+                                      1.])
+        bands = np.array([0.30, 1.00, 1.80, 3.00, 4.50, 5.20, 6.45, 7.00, 7.10, 8.00,
+                          8.10, 9.00, 9.30, 10.20, 10.50, 10.6])
+        spectrum = np.array([0.60, 1.00, 2.45, 3.10, 3.25, 4.15, 4.35, 4.10, 3.10,
+                             3.80, 2.00, 2.85, 3.75, 3.00, 2.00, 0.90])
+        assert_allclose(continuum_removed, remove_continuum(
+            spectrum, bands, mode='segmented'))
 
     def test_in_and_out_same(self):
         part = self.image[20:22, 20:22].astype(np.float64)
