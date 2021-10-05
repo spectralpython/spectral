e1cd919f5f66abddc219b76926450240feaaed8f
Thomas Boggs <thomas.boggs@gmail.com>
2021-10-05 00:03:22 -0400

    Increase version to 0.22.4 (closes #3)

--- spectral/__init__.py
+++ spectral/__init__.py
@@ -4,7 +4,7 @@ Basic package setup and global imports.
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__version__ = '0.22.3'
+__version__ = '0.22.4'
 
 import sys
 if sys.byteorder == 'little':
